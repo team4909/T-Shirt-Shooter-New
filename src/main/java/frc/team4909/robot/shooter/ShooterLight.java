8ef4c7ad2504178dcd799485e48a85dd09aecfea
Mihir Patankar <mpatankar06@gmail.com>
2021-11-16 22:00:52 -0500

    cleaned up code

--- src/main/java/frc/team4909/robot/shooter/ShooterLight.java
+++ src/main/java/frc/team4909/robot/shooter/ShooterLight.java
@@ -1,7 +1,5 @@
 package frc.team4909.robot.shooter;
 
-import com.revrobotics.CANAnalog;
-
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.AnalogInput;
@@ -11,47 +9,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class ShooterLight extends Subsystem {
 
     public static AnalogInput analogSensor;
+    
     private AddressableLED leds;
-    private AddressableLED leds2;
     private AddressableLEDBuffer ledBuffer;
-    private AddressableLEDBuffer ledBuffer2;
     private final int led_length = 138;
     
     public ShooterLight() {
         analogSensor = new AnalogInput(3);
+        
         leds = new AddressableLED(0);
-        //leds2 = new AddressableLED(2);
-
-
-
         ledBuffer = new AddressableLEDBuffer(led_length);
-        // ledBuffer2 = new AddressableLEDBuffer(80);
         leds.setLength(ledBuffer.getLength());
-        // leds2.setLength(ledBuffer2.getLength());
-
-        // for (int i = 0; i < ledBuffer2.getLength(); i++) {
-        //     ledBuffer2.setRGB(i, 255, 255, 255);
-            
-        // }
-
-    
         leds.start();
-
     }
-
- /**
- * map a number from one range to another
- * @param  {num} value   the value to be mapped
- * @param  {num} old_min the minimum of value
- * @param  {num} old_max the maximum of value
- * @param  {num} new_min the new minimum value
- * @param  {num} new_max the new maximum value
- * @return The value remaped on the range [new_min new_max]
- */
-public static double map(double value, double old_min, double old_max, double new_min, double new_max) {
-    return (value - old_min) / (old_max - old_min) * (new_max - new_min) + new_min;
-}
-
 
     @Override
     public void periodic() {
@@ -59,36 +29,42 @@ public static double map(double value, double old_min, double old_max, double ne
         double mapped_psi = map(psi, 25, 60, 0, led_length);
         if (mapped_psi > led_length) {mapped_psi = led_length;}
         if (mapped_psi < 0) {mapped_psi = 0;}
-
         {
-            
             for (int i = 0; i < mapped_psi; i++) {
                 ledBuffer.setRGB(i, 0, 255, 0);
-                
             }
             
-            
-            // SmartDashboard.putBoolean("On", true);
-
             for (int i = (int) mapped_psi; i < ledBuffer.getLength(); i++) {
                 ledBuffer.setRGB(i, 255, 0, 0);
-                
             }
-            leds.setData(ledBuffer);
             
+            leds.setData(ledBuffer);    
         }
-
+        
+        //Smart Dashboard outputs
         SmartDashboard.putNumber("PSI", psi);
         if (psi >= 60) {
-            SmartDashboard.putBoolean("On", true);
+            SmartDashboard.putBoolean("Ready", true);
         } else {
-            SmartDashboard.putBoolean("On", false);
+            SmartDashboard.putBoolean("Ready", false);
         }
     }
 
+     /**
+     * map a number from one range to another
+     * @param  {num} value   the value to be mapped
+     * @param  {num} old_min the minimum of value
+     * @param  {num} old_max the maximum of value
+     * @param  {num} new_min the new minimum value
+     * @param  {num} new_max the new maximum value
+     * @return The value remaped on the range [new_min new_max]
+     */
+    public static double map(double value, double old_min, double old_max, double new_min, double new_max) {
+        return (value - old_min) / (old_max - old_min) * (new_max - new_min) + new_min;
+    }
+    
     @Override
     protected void initDefaultCommand() {
         
     }
-    
 }
